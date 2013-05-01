b7b6150384e997d6e0ade4978baf680647c56343
Matt <matt@thebearbytes.com.au>
2013-05-02 00:03:53 +0800

    Initial commit of application
    Backs up SQL Server database from config-nominated database to
    config-nominated directory.  Logs using NLog to both console and text
    file in the "logs" directory

---
+++ BearBytes.SqlServerBackup/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace BearBytes.SqlServerBackup
+{
+    class Program
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Log.Info("Starting Database Backup Utility...");
+
+                var connectionString = ConfigurationManager.ConnectionStrings["MyConnection"].ToString();
+                var databaseName = ConfigurationManager.AppSettings["DatabaseName"];
+                var backupDir = ConfigurationManager.AppSettings["BackupDirectory"];
+
+                if(Directory.Exists(backupDir))
+                    Log.Info("Backup directory exists.");
+                else
+                {
+                    Log.Info("Backup directory doesn't exist.  Creating directory.");
+                    Directory.CreateDirectory(backupDir);
+                }
+
+                Log.Info("Backing up database '{0}' to '{1}'", databaseName, backupDir);
+
+                var backupFileName = "Backup_" + databaseName + "_" + DateTime.Now.ToString("yyyymmdd") + ".bak";
+                var backupFilePath = Path.Combine(backupDir, backupFileName);
+
+                using (var conn = new SqlConnection(connectionString))
+                {
+                    //Generate backup command
+                    var sql = string.Format("BACKUP DATABASE {0} TO DISK='{1}'", databaseName, backupFilePath);
+
+                    conn.Open();
+
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        Log.Info("Executing database backup script...");
+                        cmd.CommandText = sql;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                //Make sure that the new backup file exists
+                if (File.Exists(backupFilePath))
+                {
+                    Log.Info("Backup Successful");
+                }
+                else
+                {
+                    Log.Warn("Backup file not found after completing without errors.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal("Error: Backup aborted.  Message: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ BearBytes.SqlServerBackup/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BearBytes.SqlServerBackup")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BearBytes.SqlServerBackup")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f5137274-bc8b-4848-b700-b88f88e85a00")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
